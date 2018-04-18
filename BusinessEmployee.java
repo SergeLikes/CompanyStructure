5d846c62b10b422c68724bc34c38f4fcd5da396a
SergeLikes <38051401+SergeLikes@users.noreply.github.com>
2018-04-18 18:20:56 +0300

    Create BusinessEmployee.java

---
+++ BusinessEmployee.java
@@ -0,0 +1,22 @@
+public abstract class BusinessEmployee extends Employee {
+
+    private double bonusBudget;
+
+    public BusinessEmployee(String name, double salary) {
+        super(name,salary);
+        this.bonusBudget = 0.0;
+    }
+
+    public double getBonusBadget() {
+        return this.bonusBudget;
+    }
+
+    public void setBonusBudget(double bonusBudget) {
+        this.bonusBudget = bonusBudget;
+    }
+
+    public String employeeStatus() {
+        String result = super.toString() + " with a budget of " + this.bonusBudget;
+        return result;
+    }
+}
